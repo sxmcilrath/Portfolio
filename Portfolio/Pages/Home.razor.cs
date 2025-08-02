c7c344a2403eb97820b348578599f53342176565
sxmcilrath <samuel.xavier.mac@gmail.com>
2025-08-02 17:23:37 -0500

    Finished skills section

--- Portfolio/Pages/Home.razor.cs
+++ Portfolio/Pages/Home.razor.cs
@@ -1,4 +1,5 @@
 namespace Portfolio.Pages;
+
 public partial class Home
 {
     private Dictionary<string, string> languages = new() {
@@ -10,4 +11,30 @@ public partial class Home
         {"HTML", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/htmx/htmx-original.svg"},
         {"CSS", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"}
     };
+
+    private Dictionary<string, string> cloud = new()
+    {
+        {"Azure", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg"},
+        {"Terraform", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/terraform/terraform-original.svg"},
+        {"Docker", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg"},
+        {"DevOps", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuredevops/azuredevops-original.svg"},
+        {"Git", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"}
+    };
+
+    private Dictionary<string, string> frameworks = new()
+    {
+        {"ASP.NET CORE (Blazor)", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dotnetcore/dotnetcore-original.svg"}
+    };
+
+    private Dictionary<string, string> tools = new()
+    {
+        { "Linux (Bash)", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg" },
+        { "MAMP", "" }, 
+        { "PostgreSQL", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" },
+        { "Postman", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg" },
+        { "Swagger", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/swagger/swagger-original.svg" }, // decent fallback
+        { "Power Automate", "" }
+    };
+
+
 }

d1e325e79c87e3f587408c35d794830e38c15b29
sxmcilrath <samuel.xavier.mac@gmail.com>
2025-08-02 15:45:43 -0500

    Enhance SkillCard and Home page layout; update language icons with CDN links

--- Portfolio/Pages/Home.razor.cs
+++ Portfolio/Pages/Home.razor.cs
@@ -1,8 +1,13 @@
 namespace Portfolio.Pages;
 public partial class Home
 {
-    private Dictionary<string, string> languages = new () {
-        {"C#", "imgs/cSharp.png"},
-        {"C", "imgs/C_Logo.png"}
+    private Dictionary<string, string> languages = new() {
+        {"C#", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg"},
+        {"C", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"},
+        {"Java", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" },
+        {"Zig", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/zig/zig-original.svg"},
+        {"Python", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"},
+        {"HTML", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/htmx/htmx-original.svg"},
+        {"CSS", "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"}
     };
 }

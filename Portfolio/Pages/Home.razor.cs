654424f0687d3571ef27ae8f23b1185491773796
sxmcilrath <samuel.xavier.mac@gmail.com>
2025-08-02 15:24:14 -0500

    skill card logo base

---
+++ Portfolio/Pages/Home.razor.cs
@@ -0,0 +1,8 @@
+namespace Portfolio.Pages;
+public partial class Home
+{
+    private Dictionary<string, string> languages = new () {
+        {"C#", "imgs/cSharp.png"},
+        {"C", "imgs/C_Logo.png"}
+    };
+}

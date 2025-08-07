23880bdad83e3540b8faa76fa6d3b4683accfa4f
sxmcilrath <samuel.xavier.mac@gmail.com>
2025-08-06 20:18:53 -0500

    work timeline meh version

--- Portfolio/Components/WorkTimeline.razor.cs
+++ Portfolio/Components/WorkTimeline.razor.cs
@@ -6,32 +6,24 @@ using Microsoft.AspNetCore.Components;
 public partial class WorkTimeline
 {
     private string sdMarkdown = """
-        **Software Developer Intern** ||  *Northwestern Medicine*
-
-        * Creating responsive front-end Razor components with custom CSS for Blazor app to help automate resource provisioning and streamline dev project setup for over 1000 employees.
+        * Collaborated in an agile dev team to launch MVP 1, building .NET Razor UI components, implementing backend API controllers, provisioning Azure Cosmos DB containers and services for DTO management.
         * Deploying custom Azure modules and APIM resources to the cloud using Terraform plans and Swagger, enabling reliable infrastructure via IaC and regulation for internal and external vendor API access.
         * Developing a Razor killswitch component and RESTful .NET Core API call to automate Azure API resource deallocation and ServiceNow change requests, eliminating manual workflows.
         * Participating in an Agile DevOps environment, running CI/CD pipelines, and contributing to the SDLC.
         """;
 
     private string rsMarkdown = """
-        **Research Assistant** ||  *Wheaton College*
-
         * Worked alongside professor to migrate core components of a multichannel secret sharing protocol from C to Zig, leveraging Zig’s modern safety and concurrency features to enhance performance and maintainability.
         * Utilized stream libraries to implement message sharing across machines versus across ports.
-        * Implemented custom multithreading iPerf tests to benchmark and improve sharing speeds and throughput.
+        * Implemented custom multithreading iPerf tests to benchmark and improve sharing speeds and throughput by 10x.
         """;
 
     private string atMarkdown = """
-        **Automation Developer Intern** ||  *Northwestern Medicine*
-
         * Developed Power Automation to retrieve and store requests from ServiceNow in Dataverse, and created a custom Power App to support CRUD operations, enhancing tracking efficiency for the team and manager. App still currently in prod and is used in all shareholder meetings.
         * Developed within a team, a Power Page frontend for technicians to monitor app health and assisted backend team with data retrieval automations.
         """;
 
     private string itMarkdown = """
-        **IT Intern** ||  *STERIS/Hu-Friedy*
-
         * Met expectations with high volume tickets by closing over 250 tickets that varied from user onboards, network failures, hardware issues, etc.
         * Redesigned company's form workflows-resulting in cost savings of tens of thousands of dollars from the previous service provider, by self-teaching Power Automate.
         * Interacted frequently with both STERIS and Hu-Friedy’s Active Directory as well as Azure and other Cloud services.
